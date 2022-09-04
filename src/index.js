7dfe4878eea0cb9abbd079509f5256c1c5e5dcaf
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-09-04 16:30:55 +0200

    Pliki startowe

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './sass/index.scss';
+import Notiflix from 'notiflix';
+
+const form = documnet.querySelector('form#search-form');
+const gallery = document.querySelector('div.gallery');
+const loadMoreButton = document.querySelector('di.load-more');
+
+
+
+
