03e13827f4514d8e1de942f54fb1a3c5104882cb
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-09-05 08:47:03 +0200

    Poprawione zadanie - async/await
    Praktycznie od początku zrobione zadanie.
    
    Opis zadania:
    Stworzenie aplikacji wyszukiwania i przeglądania obrazów według słów kluczowych z użyciem publicznego API serwisu Pixabay.
    
    Dodatkowo galeria, która ładuje określoną liczbę zdjęć po kliknięciu w przycisk Load More - wykorzystanie paginacja (page / per_page)
    
    Tworzenie galerii obrazów z danych uzyskanych w odpowiedzi od serwera.
    
    Możliwe do wykorzystania biblioteki:
    
    Notiflix - do powiadomień, komenda do instalacji dla npm: npm i notiflix
    
    simplelightbox.com - do obrazów, komenda do instalacji dla npm: npm install simplelightbox
    
    Axios - bibloteka do żądań HTTP... komenda w npm: $ npm install axios
    
    Lodash - debounce
    $ {sudo -H} npm i -g npm
    $ npm i --save lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,141 +1,119 @@
-import './css/styles.css';
-import axios from 'axios';
 import Notiflix from 'notiflix';
+import axios from 'axios';
+//import debounce from 'lodash';
 import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-//axios.defaults.baseURL = 'https://pixabay.com/api/';
+const gallery = document.querySelector('.gallery');
+const input = document.querySelector('.search_input');
+const inputButton = document.querySelector('.search_btn');
+const loadMoreButton = document.querySelector('.load-more');
 
-// Zmienne DOM
-const form = document.querySelector('form#search-form');
-const gallery = document.querySelector('div.gallery');
-const searchButton = document.querySelector(`button[type="submit"]`);
-//const searchButton = document.querySelector(".search-button");
-const loadMoreButton = document.querySelector('button.load-more');
-//const loadMoreButton = document.querySelector('.load-more');
+// Właściwości zdjęć i galerii
+const API = '29707791-ff65a0300987a99cb660f7261';
+const imageType = 'photo';
+const orientation = 'horizontal';
+const safeSearch = true;
 
-form.addEventListener('submit', searchImage);
-loadMoreButton.addEventListener('click', loadMoreImages);
-
-// Zmienne
-const API_KEY = '29707791-ff65a0300987a99cb660f7261';
-const per_page = 40;
+const perPage = 40;
 let page = 1;
-let query = '';
-let lightbox = new simpleLightbox('.gallery a');
 
-// const URL = 'https://pixabay.com/api/'
+const lightbox = new SimpleLightbox('.gallery a');
 
 
-const fetchPixabay = async (query, page) => {
-    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&per_page=${per_page}&page=${page}`)
-    return response;
+loadMoreButton.setAttribute('hidden', 'hidden');
+
+
+const fetchImages = async (input, pageNumber) => {
+  const URL = `https://pixabay.com/api/?key=${API}&q=${input}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}&page=${pageNumber}&per_page=${perPage}`;
+
+  const response = await fetch(`${URL}`);
+  const responseObject = await response.json();
+
+  loadMoreButton.removeAttribute('hidden');
+  return responseObject;
 };
 
 
-function renderItems(images) {
-    let markup = images
-        .map(image => {
-            const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image
-      
-    return `<a class="img_card" href='${largeImageURL}'> 
-              <div class="photo-card" id= "${id}"> 
-                <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy" /> 
-              <div class="info">
-                <p class="info-item">
-                  <b>likes</b>${likes}
-                </p>
-                <p class="info-item">
-                  <b>views</b>${views}
-                </p>
-                <p class="info-item">
-                  <b>comments</b>${comments}
-                </p>
-                <p class="info-item">
-                  <b>downloads</b>${downloads}
-                </p>
-              </div>
-            </div>
-          </a>`
-})
-    .join("");
+const renderImages = images => {
+  const markup = images
+    .map(image => `
+      <div class="photo-card">
+        <a href='${image.largeImageURL}'>
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${image.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b> ${image.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b> ${image.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${image.downloads}
+          </p>
+        </div>
+      </div>`
+    )
+    .join('');
+
+  if (page === 1) {
+    gallery.innerHTML = markup;
+  } else {
     gallery.insertAdjacentHTML('beforeend', markup);
-}
-
-loadMoreButton.addEventListener('click', onloadMoreButton)
-form.addEventListener('submit', searchForm)
-
-
-async function searchForm(e) {
-  try { 
-    e.preventDefault();
-    page = 1;
-    query = e.currentTarget.searchQuery.value.trim();
-    
-    if (query === '') {
-      Notiflix.Notify.failure(`Oops, the search input cannot be empty`)
-    return;
-    }
-    gallery.innerHTML = '';
-    loadMoreButton.classList.add('is-hidden');
-
-    const images = await fetchPixabay(query, page);
-    const data = images.data;
-
-    if (data.totalHits === 0) {
-      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    } else {
-      renderItems(data.hits);
-      lightbox.refresh();
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      onSearchNotification(data);
   }
-} 
-  catch (error) {
-    gallery.innerHTML = '';
-    console.log(error);
-  }
-}
+  return page++;
+};
 
 
-async function onloadMoreButton() {
-  page++;
+inputButton.addEventListener('click', async event => {
+  event.preventDefault();
+
+  page = 1;
+  const inputValue = input.value.trim();
+
   try {
-    const images = await fetchPixabay(query, page);
-    const data = images.data;
-    renderItems(data.hits);
-    lightbox.refresh();
-    onSearchNotification(data)
-    loadMoreButton.classList.add('is-hidden');
-  }
-  catch(error) {
-    console.log(error)
-  }
-}
+    const array = await fetchImages(inputValue, page);
+    const arrayImages = [];
 
+    array.hits.forEach(async image => {
+      arrayImages.push(image);
+    });
 
-function onSearchNotification(data) {
+    const total = await array.totalHits;
 
-  const totalPages = Math.ceil(data.totalHits / per_page);
-
-  if (page >= totalPages) {
-    loadMoreButton.classList.add('is-hidden');
-    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
-    return
-  }
-  
-  if (data.totalHits === 0) {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-  }
-
-  window.onscroll = function () {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      // you're at the bottom of the page
-      console.log('end of page')
-      loadMoreButton.classList.remove('is-hidden')
-    } else {
-      loadMoreButton.classList.add('is-hidden')
+    if (total > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${total} images.`);
     }
-  }
-}
 
+    if (total === 0) {
+      throw new Error();
+    }
+    renderImages(arrayImages);
+    lightbox.refresh();
+  } catch (error) {
+    gallery.innerHTML = '';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+});
+
+loadMoreButton.addEventListener('click', async () => {
+  const inputValue = input.value.trim();
+  try {
+    const array = await fetchImages(inputValue, page);
+    const arrayImages = [];
+
+    array.hits.forEach(async image => {
+      arrayImages.push(image);
+    });
+    renderImages(arrayImages);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
